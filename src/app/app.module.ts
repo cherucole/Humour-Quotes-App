27459b0a8398416d7e3512b22a47e27ef8290a45
cherucole <cheruyotcheruyot@gmail.com>
2018-08-11 15:02:29 +0300

    files initialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuoteFormComponent } from './quote-form/quote-form.component';
+import { QuoteDisplayComponent } from './quote-display/quote-display.component';
+import { QuoteVoteComponent } from './quote-vote/quote-vote.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuoteFormComponent,
+    QuoteDisplayComponent,
+    QuoteVoteComponent
   ],
   imports: [
     BrowserModule
